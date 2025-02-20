65774ebcd60df1cd69cce64557b32e72fdd4c97b
HoangQuach <quachhoangqb@gmail.com>
2025-02-20 16:18:50 +0700

    Implement the CI (#1)
    * Tet
    
    * Update yaml
    
    * Update yaml
    
    * Fix
    
    * Print vars
    
    * etst
    
    * Update yaml

--- LandingV1/app/src/main/java/com/hoangquach/landingv1/MainActivity.kt
+++ LandingV1/app/src/main/java/com/hoangquach/landingv1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             LandingV1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android 2",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
